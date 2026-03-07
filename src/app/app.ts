fab9781d4af2ed06618c91a70436ca05b239611c
Andrea Robles Davila <82170461+itsarodav@users.noreply.github.com>
2026-03-07 20:00:51 +0100

    release-card fixed and styles created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReleaseCard } from './shared/components/release-card/release-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReleaseCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/shared/components/release-card/release-card.component.ts
+++ src/app/shared/components/release-card/release-card.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 export type ReleaseType = 'Single' | 'EP' | 'Álbum';
 
 @Component({
   selector: 'app-release-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    AngularSvgIconModule,
+  ],
   templateUrl: './release-card.component.html',
   styleUrl: './release-card.component.scss',
 })
