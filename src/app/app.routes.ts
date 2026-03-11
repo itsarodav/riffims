87348a33f5dd94b061f5f197c6c5e77c40ba9733
Andrea Robles Davila <82170461+itsarodav@users.noreply.github.com>
2026-03-11 22:45:23 +0100

    auth-layout component added and fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' }
+];
