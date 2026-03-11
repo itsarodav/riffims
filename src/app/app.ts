25fff500b476d9a40b2c6c83cc0475ab552765f6
Andrea Robles Davila <82170461+itsarodav@users.noreply.github.com>
2026-03-11 22:54:12 +0100

    app.ts updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReleaseCardComponent } from './shared/components/release-card/release-card.component';
-import { MultiCardComponent } from './shared/components/multi-card/multi-card.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
-    ReleaseCardComponent,
-    MultiCardComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
