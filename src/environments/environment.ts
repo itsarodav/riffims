4dddbbe2520456a9a1ffb53a226809cf955a8b33
Andrea Robles Davila <82170461+itsarodav@users.noreply.github.com>
2026-03-30 22:04:51 +0200

    fix: environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    supabaseUrl: process.env['SUPABASE_URL'] ?? '',
-    supabaseKey: process.env['SUPABASE_ANON_KEY'] ?? ''
+    supabaseUrl: 'https://vbixzpdhyjqsbeebampf.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaXh6cGRoeWpxc2JlZWJhbXBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NDE1MjgsImV4cCI6MjA4ODMxNzUyOH0.4R3SY3Zd7K1gGf-0eJhbyKkGrij0MyIVBhSTvDw-LIQ'
 };
